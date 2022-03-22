3cfaddf77f7cee458d10ba830205cd1bd856f921
Shinji Tanaka <stanaka@mercari.com>
2022-03-22 09:41:38 +0900

    update for new m1 macbook pro

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ set -ev
 SRC_DIRECTORY="$HOME/src/github.com/stanaka"
 ANSIBLE_DIRECTORY="$SRC_DIRECTORY/ansible-playbook-osx"
 ANSIBLE_CONFIGURATION_DIRECTORY="$HOME/.ansible.d"
+BREW_BASEDIR="/opt/homebrew"
 
 # Download and install Command Line Tools
 if [[ ! -x /usr/bin/gcc ]]; then
@@ -13,39 +14,17 @@ if [[ ! -x /usr/bin/gcc ]]; then
 fi
 
 # Download and install Homebrew
-if [[ ! -x /usr/local/bin/brew ]]; then
+if [[ ! -x $BREW_BASEDIR/bin/brew ]]; then
     echo "Info   | Install   | homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Modify the PATH
-export PATH=/usr/local/bin:$PATH
-
-# Download and install zsh
-# if [[ ! -x /usr/local/bin/zsh ]]; then
-#     echo "Info   | Install   | zsh"
-#     brew install zsh
-# fi
-
-# # Download and install git
-# if [[ ! -x /usr/local/bin/git ]]; then
-#     echo "Info   | Install   | git"
-#     brew install git
-# fi
-
-# # Download and install python
-# if [[ ! -x /usr/local/bin/python ]]; then
-#     echo "Info   | Install   | python"
-#     brew install python --framework --with-brewed-openssl
-# fi
-
-# # Download and install hg
-# if [[ ! -x /usr/local/bin/hg ]]; then
-#     pip install mercurial
-# fi
+export PATH=$BREW_BASEDIR/bin:$PATH
 
 # Download and install Ansible
-if [[ ! -x /usr/local/bin/ansible ]]; then
+if [[ ! -x $BREW_BASEDIR/bin/ansible ]]; then
     brew install ansible
 fi
 
@@ -54,20 +33,6 @@ fi
 if [[ ! -d $SRC_DIRECTORY ]]; then
     mkdir -p $SRC_DIRECTORY
 fi
-
-# # Clone down ansible
-# if [[ ! -d $ANSIBLE_DIRECTORY ]]; then
-#     git clone -b devel git://github.com/danieljaouen/ansible.git $ANSIBLE_DIRECTORY
-# fi
-
-# # Use the forked Ansible
-# source $ANSIBLE_DIRECTORY/hacking/env-setup > /dev/null
-
-# # Clone down the Ansible repo
-# if [[ ! -d $ANSIBLE_CONFIGURATION_DIRECTORY ]]; then
-#     git clone git@bitbucket.org:danieljaouen/ansible-base-box.git $ANSIBLE_CONFIGURATION_DIRECTORY
-#     (cd $ANSIBLE_CONFIGURATION_DIRECTORY && git submodule init && git submodule update)
-# fi
 
 # # Provision the box
 # ansible-playbook --ask-sudo-pass -i $ANSIBLE_CONFIGURATION_DIRECTORY/inventories/osx $ANSIBLE_CONFIGURATION_DIRECTORY/site.yml --connection=local
---
+++ create_symlinks.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+BASEDIR=$HOME/OneDrive/lib/dotfiles
+ZSH_BASEDIR=$HOME/OneDrive/lib/zsh
+
+for i in .bash_profile .bashrc .emacs.d .gitconfig .gitignore .tmux.conf;
+do
+    if [ ! -e $BASEDIR/$i ]; then
+        echo ln -s $BASEDIR/$i $HOME
+    fi
+done
+
+for i in .zshrc .zsh-history;
+do
+    if [ ! -e $ZSH_BASEDIR/$i ]; then
+        echo ln -s $ZSH_BASEDIR/$i $HOME
+    fi
+done
